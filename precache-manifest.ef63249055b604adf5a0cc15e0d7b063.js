2cfa3262aa6b3f16406f364d26ddd895593c03f3
Symon Hasan <isymonhs@gmail.com>
2020-05-17 16:43:54 +0600

    Updates

---
+++ precache-manifest.ef63249055b604adf5a0cc15e0d7b063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3533526a66bac8ae29b23da5bdeb7b",
+    "url": "/cgpa/index.html"
+  },
+  {
+    "revision": "1275713df150ebbb4ede",
+    "url": "/cgpa/static/css/main.146d682b.chunk.css"
+  },
+  {
+    "revision": "f79d6abea154e76c2f9e",
+    "url": "/cgpa/static/js/2.543d740a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cgpa/static/js/2.543d740a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1275713df150ebbb4ede",
+    "url": "/cgpa/static/js/main.3e981dfa.chunk.js"
+  },
+  {
+    "revision": "31a5bc0c3fdf473794f9",
+    "url": "/cgpa/static/js/runtime-main.536e797c.js"
+  }
+]);
